302b695db36eee1fd695e6901809acbe4ee684fe
mikatalk <mikatalk@users.noreply.github.com>
2020-02-24 13:57:54 -0800

    Deploy to tips4devs.com

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' 
-    ? '//mikatalk.github.io/vector-flows/2d/'
+    // ? '//mikatalk.github.io/vector-flows/2d/'
+    ? '/article-assets/visualize-2d-and-3d-vector-fields-with-three-js/2d/'
     : `/`,
   devServer: {
     hot: false,
